6037fde767ce8cdcdf7648e0f313e6bc5e3b0fcb
Franck LEBAS <franck.lebas@betao.se>
2024-05-10 19:33:22 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,12 @@ cd yay
 makepkg -si
 
 printf "Installing stow\n"
-sudo pacman -S stow zsh neovim ripgrep fd lazygit git curl make wget tmux firefox neovim docker alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-arandr xorg-xrandr -y
+sudo pacman -S stow zsh neovim ripgrep fd git curl make wget tmux firefox neovim docker alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-arandr xorg-xrandr -y
+
+printf "Install aur packages"
+yay lazygit
+yay brave-bin
+yay enpass-bin
 
 printf "Installing tmux tmp\n"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

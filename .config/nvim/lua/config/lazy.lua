ffd10269addd588cef9d34457e45c9294aa1c7c6
Franck LEBAS <franck.lebas@betao.se>
2024-05-28 10:23:53 +0200

    feat: config i3

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "rose-pine",
+        colorscheme = "solarized-osaka",
         news = {
           lazyvim = true,
           neovim = true,

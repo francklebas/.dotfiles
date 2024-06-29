ce377fa10292cad3ddb4071fac01fa1bca5d4dcb
Franck LEBAS <franck.lebas@betao.se>
2024-06-29 13:28:59 +0200

    feat: remove unused fonts

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 sudo pacman -Syu -y
 
+printf "Installing stow\n"
+sudo pacman -S git btop htop neofetch stow zsh vim neovim ripgrep fd lazygit git curl make wget tmux firefox neovim alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-xrandr pavucontrol -y
+
+printf "Installing tmux tmp\n"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 printf "Installing yay\n"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
-printf "Installing stow\n"
-sudo pacman -S btop htop neofetch stow zsh vim neovim ripgrep fd lazygit git curl make wget tmux firefox neovim alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-xrandr pavucontrol -y
-
-printf "Installing tmux tmp\n"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 printf "Installing aur packages\n"
 yay docker-desktop

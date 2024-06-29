a8a7ac98376792a856de94d492df5edd0fc4a072
Franck LEBAS <franck.lebas@betao.se>
2024-06-29 13:23:55 +0200

    feat: update installer

--- install.sh
+++ install.sh
@@ -8,15 +8,20 @@ cd yay
 makepkg -si
 
 printf "Installing stow\n"
-sudo pacman -S stow zsh neovim ripgrep fd lazygit git curl make wget tmux firefox neovim docker alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-xrandr -y
+sudo pacman -S btop htop neofetch stow zsh vim neovim ripgrep fd lazygit git curl make wget tmux firefox neovim alacritty i3-wm i3blocks i3status i3lock polybar picom dmenu rofi feh blueman bluez bluez-utils bluez-tools brightnessctl ttf-font-awesome font-manager dunst xorg-xrandr pavucontrol -y
 
 printf "Installing tmux tmp\n"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-printf "Installing docker desktop\n"
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
-sudo systemctl start docker
+printf "Installing aur packages\n"
+yay docker-desktop
+yay google-chrome
+yay brave-bin
+yay enpass-bin
+yay slack-desktop
+yay pika-backup
+yay timeshift
+yay vlc
 
 printf "Installing oh-my-zsh\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

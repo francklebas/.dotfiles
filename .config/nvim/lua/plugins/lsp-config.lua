8657e8c46ae94574f80445a39d13416d7e40336e
Franck LEBAS <franck.lebas@betao.se>
2025-03-31 08:29:01 +0200

    feat(wallapaper)

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -13,12 +13,26 @@ lspconfig.volar.setup({
   },
 })
 
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   -- Exclure les fichiers .vue
   on_attach = function(client, bufnr)
     if vim.bo[bufnr].filetype == "vue" then
       client.stop()
     end
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   end,
+  init_options = {
+    preferences = {
+      includeCompletionsForModuleExports = true,
+      includeCompletionsWithInsertText = true,
+    },
+    plugins = {
+      {
+        name = "@adonisjs/ts-plugin",
+        location = vim.fn.stdpath("data") .. "/mason/packages/typescript-language-server/node_modules", -- adapte si besoin
+      },
+    },
+  },
 })
